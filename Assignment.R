2017762df107328c9d05ae12ea2efabb2d7040f3
FHGRollic <john.conducto@obf.ateneo.edu>
2021-07-12 18:07:17 +0800

    1st Assignment

---
+++ Assignment.R
@@ -0,0 +1,3 @@
+print("Hello,I am John Rollic V. Conducto from BS EcE")
+print( "I actually look forward into this class since data is something that I really wonder about; what is it? is it a matter? an electric charge? does digital data has mass or does it occupy space?")
+print (" It just really mesmerizes me how such diminutive objects runs the world now")
